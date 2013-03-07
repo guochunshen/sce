ce7c54ed377c584784ce9bebd85f1f53f660c027
guochun <shenguochun@gmail.com>
2013-03-07 13:56:02 +0800

    修正了sigHabitatTest中参数顺序颠倒的问题，同时完善了有关fitCluster中ctlpars参数的解释

--- R/sigHabitatTest.R
+++ R/sigHabitatTest.R
@@ -30,8 +30,8 @@ sigHabitatTest<-function(fittedModel,clusterResidual=TRUE){
   }
     data.ppm=attr(fittedModel,"fittedmodel")
     nu=fittedModel[1]
-    alpha=fittedModel[2]
-    sigma2=fittedModel[3]
+    alpha=fittedModel[3]
+    sigma2=fittedModel[2]
     beta=fittedModel[-c(1:3)]
     if(clusterResidual)
       acacov=vcov.mykppm(data.ppm,par=c(sigma2,alpha),nu=nu)
