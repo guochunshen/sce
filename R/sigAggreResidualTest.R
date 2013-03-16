dae773e418b5fac12147d8a29d3d5ca220e6460b
guochun-pc <shenguochun@gmail.com>
2013-03-16 19:30:41 +0800

    修改了sigAggreResidualTest的pcf计算方法，现在使用内存更小，速度更快

--- R/sigAggreResidualTest.R
+++ R/sigAggreResidualTest.R
@@ -20,26 +20,60 @@
 
 sigAggreResidualTest=function(fittedmodel,nsim=10,r=seq(0,60,2),edcor="translate"){
   data=attr(fittedmodel,"data")
+  xy=data.frame(x=data$com$x,y=data$com$y)
   data.ppm=attr(fittedmodel,"fittedmodel")
+  bw=attr(fittedmodel,"ctlpars")$bw
+  npp=nsim+1
+  
   #since the translate edge correction need very large memory in its calculation, thus using border edge correction
   #instand is the number of points large than 3000
-  if(data$N<3000){
-    e=envelope(data.ppm,nsim=nsim,verbose=FALSE,savefuns=TRUE,correction=edcor,r=r)
-  }else{
-    e=envelope(data.ppm,nsim=nsim,verbose=FALSE,savefuns=TRUE,correction="border",r=r)
+#   if(data$N<3000){
+#     e=envelope(data.ppm,fun=pcf,nsim=nsim,verbose=FALSE,savefuns=TRUE,correction=edcor,r=r)
+#   }else{
+#     e=envelope(data.ppm,fun=pcf,nsim=nsim,verbose=FALSE,savefuns=TRUE,correction="border",r=r)
+#   }
+#   Kfuns=attr(e,"simfuns")
+#   n=length(e$obs)
+#   Kfuns[,1]=e$obs
+  
+  lambda_loc=predict.ppm(data.ppm,locations=xy)
+  pcf_obs=pcf_adaptive(data$com,lambda=lambda_loc,maxt=max(r),bw=bw,adaptive =FALSE)
+  
+  pcf_r=pcf_obs$r
+  pcf_obs=pcf_obs$trans
+  #plot(x=pcf_r,y=pcf_obs)
+  
+  Kfuns=matrix(nrow=length(pcf_obs),ncol=npp)
+  lambda=predict.ppm(data.ppm)
+  for(i in 1:nsim){
+    data_simu=rpoispp(lambda)
+    Kfuns[,i+1]=pcf_adaptive(data_simu,lambda=lambda_loc,maxt=max(r),bw=bw,adaptive =FALSE)$trans
+    while(any(is.infinite(Kfuns[,i+1]))){
+      data_simu=rpoispp(lambda)
+      Kfuns[,i+1]=pcf_adaptive(data_simu,lambda=lambda_loc,maxt=max(r),bw=bw,adaptive =FALSE)$trans
+    }
+     
+    #lines(x=pcf_r,y=Kfuns[,i+1],col=2)
   }
   
-  npp=nsim+1
-  Kfuns=attr(e,"simfuns")
-  n=length(e$obs)
-  Kfuns[,1]=e$obs
-  ui=vector("numeric",npp)
-  for (k in 2:(n-1)){
-    Kit=Kfuns[k,]
-    t.rslt=step.ui(Kit,2,npp)
-    ui = ui+t.rslt
-  }
-  pvalue=calc.pval(ui)
+  Kfuns[,1]=pcf_obs
+  n=length(pcf_obs)
+  
+  hsum=apply(Kfuns,1,sum,na.rm=TRUE)
+  hmean=(hsum-Kfuns)/nsim
+  
+  ui=apply((Kfuns-hmean)^2,2,sum,na.rm=TRUE)
+  pvalue=1-sum(ui[1]>ui[-1])/nsim
+
+#   
+#   
+#   ui=vector("numeric",npp)
+#   for (k in 2:(n-1)){
+#     Kit=Kfuns[k,]
+#     t.rslt=step.ui(Kit,2,npp)
+#     ui = ui+t.rslt
+#   }
+#   pvalue=calc.pval(ui)
   names(pvalue)="aggreRes"
   return(pvalue)
 }
