a91544fc35e5be91b9c60f51a69c074415925cca
guochun-pc <shenguochun@gmail.com>
2013-05-07 13:58:23 +0800

    修改了gnonrandomDPDE的计算方式，对event to event 的距离采用了新的种群密度统计量。

--- R/gnonrandomDPDE.R
+++ R/gnonrandomDPDE.R
@@ -12,6 +12,12 @@
 #'different.
 #'
 #'
+#'@examples
+#'
+#'r_samples=matrix(runif(100,0,4),nrow=50,ncol=2)
+#'gnonrandomDPDE(r_samples,dtype="ptoe",k=1,area=100)
+#'gnonrandomDPDE(r_samples,dtype="etoe",k=1,area=100)
+#'
 
 
 #TODO add the etoe estimator into the function
@@ -26,7 +32,12 @@ gnonrandomDPDE<-function(r_samples,dtype="ptoe",k,area){
   #remove 0 neighborhood distance
   r=r[r!=0]
   if(length(r)>0){
-    ngpars=try(optim(c(0.03,2),fn=ngobf,r=r,k=k,q=q,control=list(maxit=100000,fnscale=-1)))
+    if(dtype=="ptoe"){
+      ngpars=try(optim(c(0.03,2),fn=ngobf_ptoe,r=r,k=k,q=q,control=list(maxit=100000,fnscale=-1)))
+    }else{
+      ngpars=try(optim(c(0.03,2),fn=ngobf_etoe,r=r,k=k,q=q,control=list(maxit=100000,fnscale=-1)))
+    }
+    
     if(class(ngpars)=="try-error"){
       #browser()
       N=NA
@@ -40,7 +51,7 @@ gnonrandomDPDE<-function(r_samples,dtype="ptoe",k,area){
   return(N)
 }
 
-ngobf=function(x,r,k,q){
+ngobf_ptoe=function(x,r,k,q){
   #x=c("lambda","a")
   if(any(x<0))
     return(-Inf)
@@ -50,5 +61,19 @@ ngobf=function(x,r,k,q){
   #re=n*log(lam)+sum(log(r))-(k+1)*sum(log(1+(pi*lam*r^2)/k))
   re=n*log(lam)+n*lgamma(k+a)+n*a*log(a)-n*lgamma(a)+
     sum(log(r)+(k-1)*log(pi*lam/q*r^2)-(k+a)*log(pi*lam/q*r^2+a))
+  return(re)
+}
+
+
+ngobf_etoe=function(x,r,k,q){
+  #x=c("lambda","a")
+  if(any(x<0))
+    return(-Inf)
+  lam=x[1]
+  a=x[2]
+  n=length(r)
+  
+  re=n*log(lam)+n*lgamma(k+a+1)+n*(a+1)*log(a)-n*lgamma(a+1)+
+    sum(log(r)+(k-1)*log(pi*lam/q*r^2)-(k+a+1)*log(pi*lam/q*r^2+a))
   return(re)
 }
