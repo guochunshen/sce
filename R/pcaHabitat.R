ae15e5272ff368a3ad80fb4710f49a62e93e004a
guochun <shenguochun@gmail.com>
2013-05-25 19:50:12 +0800

    增加了一个环境pca转换函数

---
+++ R/pcaHabitat.R
@@ -0,0 +1,18 @@
+#'
+#' PCA transformation of the habitat variables
+#'
+#'@param habitat a list of im with the same dim
+#'@param scale scale all variables under the unit variance.
+#'
+
+
+pcaHabitat=function(habitat,scale=TRUE){
+  n=length(habitat)
+  da=data.frame(habitat[[1]]$v)
+  for(i in 2:n){
+    da=cbind(da,habitat[[i]]$v)
+  }
+  pcaall=scores(rda(da,scale=scale),choices=1:n)$sites
+  covr=dataframeToIms(pcaall,dim(habitat[[1]]),habitat[[1]]$xcol,habitat[[1]]$yrow)
+  return(covr)
+}
