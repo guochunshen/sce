f7027f5c42b6984695ad261a81b847c676853f49
guochun-pc <shenguochun@gmail.com>
2013-06-13 16:15:56 +0800

    改进了as.scp

--- R/as.scp.R
+++ R/as.scp.R
@@ -5,9 +5,14 @@
 #'
 
 as.scp<-function(data.ppp){
-  if(is.data.frame(data.ppp$marks))
-    sp=as.vector(data.ppp$marks[,1])
-  else
-    sp=as.vector(data.ppp$marks)
+  if(is.null(data.ppp$marks)){
+    sp=rep(1,data.ppp$n)
+  }else{
+    if(is.data.frame(data.ppp$marks))
+      sp=as.vector(data.ppp$marks[,1])
+    else
+      sp=as.vector(data.ppp$marks)
+  }
+  
   return(scp(species=sp,x=data.ppp$x,y=data.ppp$y,win=data.ppp$window))
 }
