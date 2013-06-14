4903b4122084266726c61efcc0ee752cc316febc
guochun <shenguochun@gmail.com>
2013-06-14 08:09:15 +0800

    纠正了一个pcahabitat的一个错误

--- R/pcaHabitat.R
+++ R/pcaHabitat.R
@@ -8,9 +8,9 @@
 
 pcaHabitat=function(habitat,scale=TRUE){
   n=length(habitat)
-  da=data.frame(habitat[[1]]$v)
+  da=data.frame(as.vector(habitat[[1]]$v))
   for(i in 2:n){
-    da=cbind(da,habitat[[i]]$v)
+    da=cbind(da,as.vector(habitat[[i]]$v))
   }
   pcaall=scores(rda(da,scale=scale),choices=1:n)$sites
   covr=dataframeToIms(pcaall,dim(habitat[[1]]),habitat[[1]]$xcol,habitat[[1]]$yrow)
